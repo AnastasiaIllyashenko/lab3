205048772b4b7301f90cd99f77f8ad8f35add272
AnastasiaIllyashenko <119579234+AnastasiaIllyashenko@users.noreply.github.com>
2023-05-24 15:12:28 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double epsilon = 0.0001; // Задана похибка
+
+        double sum = 0.0;
+        double term = 0.0;
+        double i = 1.0;
+
+        do
+        {
+            term = 1.0 / (i * i); // Обчислення доданку
+            sum += term; // Додавання доданку до суми
+            i++;
+        } while (term >= epsilon); // Перевірка похибки
+
+        Console.WriteLine("Наближене значення суми: " + sum);
+    }
+}
